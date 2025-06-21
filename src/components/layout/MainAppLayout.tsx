ae1f1b51a6c41758e832b19d77257bf3200aa345
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-21 17:39:08 +0530

    Files generated: MainAppLayout.tsx

---
+++ src/components/layout/MainAppLayout.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import Header from './Header';
+
+interface MainAppLayoutProps {
+  children: React.ReactNode;
+}
+
+const MainAppLayout: React.FC<MainAppLayoutProps> = ({ children }) => {
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[256px_1fr]">
+      <Sidebar />
+      <div className="flex flex-col">
+        <Header />
+        <main className="flex-1 overflow-y-auto bg-background p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default MainAppLayout;
